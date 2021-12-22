b7b5e7baf236c904d8c49004d4f19ea9ee37976e
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-22 19:59:53 -0400

    Create Product endpoint created

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
     price = models.IntegerField()
+
+    def __str__(self):
+        return f"Product {self.name}, {self.price}$"
--- apps/inventory/tests/test_views.py
+++ apps/inventory/tests/test_views.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from django.urls import reverse
 
+from apps.inventory.models import Product
+
 
 class ProductListTests(TestCase):
     def setUp(self):
@@ -11,3 +13,39 @@ class ProductListTests(TestCase):
     def test_list(self):
         response = self.app.get(self.url)
         self.assertEqual(response.status_code, 200)
+        # todo: verify product info is in the response
+
+
+class ProductCreateTests(TestCase):
+    def setUp(self):
+        self.app = APIClient()
+        self.url = reverse("inventory:product-list")
+        self.data = {
+            "name": "fake_name",
+            "price": 12
+        }
+
+    def test_create(self):
+        count = Product.objects.count()
+        response = self.app.post(self.url, data=self.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Product.objects.count(), count + 1)
+
+        self.assertEqual(response.data["name"], self.data["name"])
+        self.assertEqual(response.data["price"], self.data["price"])
+
+        self.assertTrue(Product.objects.filter(**self.data).exists())
+
+    def test_create_repeated_name_should_fail(self):
+        product = Product.objects.create(name="fake", price=10)
+        self.data["name"] = product.name
+
+        count = Product.objects.count()
+
+        response = self.app.post(self.url, data=self.data)
+        self.assertEqual(response.status_code, 400)
+
+        self.assertEqual(Product.objects.count(), count)
+
+
+
