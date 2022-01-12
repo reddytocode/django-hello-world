0f6fa7c409a160a032d3b00bca2be372bfb5ff36
Reddy Tintaya <aaabeeelooon@gmail.com>
2022-01-12 18:49:48 -0400

    db models

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -7,3 +8,25 @@ class Product(models.Model):
 
     def __str__(self):
         return f"Product {self.name}, {self.price}$"
+
+
+class ProductInventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="inventory")
+    quantity = models.PositiveIntegerField(validators=(MinValueValidator(0), ))
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_orders")
+    order = models.ForeignKey("Order", on_delete=models.CASCADE, related_name="product_orders")
+
+
+class Order(models.Model):
+    order_number = models.IntegerField(unique=True)
+    client_name = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product, through=ProductOrder, related_name="orders")
+
+    def add_product(self, product) -> bool:
+        if product in list(self.products.all()):
+            return False
+        self.products.add(product)
+        return True
--- apps/inventory/tests/test_models.py
+++ apps/inventory/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.db import IntegrityError
 from django.test import TestCase
 
-from apps.inventory.models import Product
+from apps.inventory.models import Product, ProductInventory, Order, ProductOrder
 
 
 class ProductModelTests(TestCase):
@@ -23,3 +23,61 @@ class ProductModelTests(TestCase):
 
         with self.assertRaises(IntegrityError):
             Product.objects.create(name=name, price=4)
+
+    def test_delete_product(self):
+        product = Product.objects.create(name="fake_product", price=1)
+        ProductInventory.objects.create(product=product, quantity=10)
+
+        product_count = Product.objects.count()
+        p_i_count = ProductInventory.objects.count()
+
+        product.delete()
+        self.assertEqual(Product.objects.count(), product_count - 1)
+        self.assertEqual(ProductInventory.objects.count(), p_i_count - 1)
+
+
+class ProductInventoryModelTests(TestCase):
+    def test_create(self):
+        product = Product.objects.create(name="fake_product", price=1)
+        count = ProductInventory.objects.count()
+        ProductInventory.objects.create(product=product, quantity=10)
+        self.assertEqual(ProductInventory.objects.count(), count + 1)
+        self.assertTrue(ProductInventory.objects.filter(product=product, quantity=10).exists())
+
+    def test_quantity_negative(self):
+        product = Product.objects.create(name="fake_product", price=1)
+        wrong_quantity = -1
+        with self.assertRaises(IntegrityError):
+            ProductInventory.objects.create(product=product, quantity=wrong_quantity)
+
+
+class OrderModelTests(TestCase):
+    def test_create(self):
+        order_number = "123"
+        client_name = "Fake client"
+        count = Order.objects.count()
+
+        order = Order.objects.create(order_number=order_number, client_name=client_name)
+        self.assertEqual(Order.objects.count(), count + 1)
+        self.assertTrue(Order.objects.filter(order_number=order_number, client_name=client_name).exists())
+        order.delete()
+
+    def test_add_product(self):
+        order_number = "123"
+        client_name = "Fake client"
+        order = Order.objects.create(order_number=order_number, client_name=client_name)
+        product = Product.objects.create(name="fake_product", price=1)
+
+        count = ProductOrder.objects.count()
+        response = order.add_product(product)
+
+        self.assertTrue(response)
+        self.assertEqual(ProductOrder.objects.count(), count+1)
+
+        self.assertTrue(ProductOrder.objects.filter(product=product, order=order).exists())
+
+        # deberia fallar
+        response = order.add_product(product)
+        self.assertFalse(response)
+        self.assertEqual(ProductOrder.objects.count(), count+1)
+
