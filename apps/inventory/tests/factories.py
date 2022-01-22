a27a23c5d45f1418a8bc683199676407c0dad418
Reddy Tintaya <aaabeeelooon@gmail.com>
2022-01-22 11:43:39 -0400

    Add todo

--- apps/inventory/tests/factories.py
+++ apps/inventory/tests/factories.py
@@ -1,11 +1,21 @@
 import factory
-
-from apps.inventory.models import Product
+from apps.inventory.models import Product, ProductInventory
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
     name = factory.Sequence(lambda n: f"product-{n}")
-    price = factory.Sequence(lambda n: n)
+    price = factory.Faker("pyint", min_value=0, max_value=100)
 
+
+class ProductInventoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ProductInventory
+    product = factory.SubFactory(ProductFactory)
+    quantity = factory.Faker("pyint", min_value=0, max_value=100)
+
+# TODO: CREATE factory for Product Order and Order
+# order
+# name = factory.Faker("name")
+# no se necesita el product
